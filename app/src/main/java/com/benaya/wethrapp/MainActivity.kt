80fd279856c1e1d51f06b5ee34ec4427d8dc9a5b
David <0544354125m@gmail.com>
2020-12-09 11:27:30 +0200

    preparation with manual inseration

---
+++ app/src/main/java/com/benaya/wethrapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.benaya.wethrapp
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("ResourceType")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_2)
+
+
+    }
+}
